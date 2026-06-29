89ec581cdfe159cf3a9295bcce4fdee0c898ea04
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2026-06-29 13:48:06 -0400

    Support.app Extensions

--- NetworkInfo.zsh
+++ NetworkInfo.zsh
@@ -18,22 +18,18 @@ colorIndicator="true"
 
 # Setup UI indicators
 if [[ "$colorIndicator" == "true" ]]; then
-    greenCircle="🟢"
-    yellowCircle="🟡 "
-    redCircle="🔴"
+    greenCircle="🟢 " yellowCircle="🟡 " redCircle="🔴 "
 else
-    greenCircle=""
-    yellowCircle=""
-    redCircle=""
+    greenCircle="" yellowCircle="" redCircle=""
 fi
 
 # Default status and alert level
-typeset -g ShowAlert=false
+declare -g ShowAlert=false
 
 # Initialize global variables for symbol and return value. These will be updated by the getNetworkStatus function based on the current network status. 
 # The symbol variable will be used to set the appropriate SF Symbol icon in the Support App, and the retval variable will contain the text output (IP address and connection type or an error message).
-typeset -g retval="No active adapter found"
-typeset -g symbol="network.slash"
+declare -g retval="No active adapter found"
+declare -g symbol="network.slash"
 
 function getNetworkStatus()
 {
@@ -64,7 +60,7 @@ function getNetworkStatus()
     # 2. Check Ethernet (Prioritize wired)
     # Find active services and filter for Ethernet-like names
     # If found, retrieve the IP address and set the symbol to a desktop computer to indicate wired connection.
-    local eth_dev=$(networksetup -listnetworkserviceorder | awk -F'Device: ' '/Ethernet|LAN/ {print $2}' | tr -d ')')
+    local eth_dev=$(networksetup -listnetworkserviceorder | awk -F 'Device: ' '/Ethernet|LAN/ {print $2}' | tr -d ')')
     for dev in ${(f)eth_dev}; do
         ip=$(ipconfig getifaddr "$dev" 2>/dev/null)
         if [[ -n "$ip" ]]; then
@@ -87,21 +83,20 @@ function getNetworkStatus()
 
 # Set the loading key to true to trigger the loading animation in the Support App while we retrieve the battery information
 defaults write "$supportAppDir" "${extensionID}_loading" -bool true
-sleep 0.5
+sleep 0.25
 
 # Fetch network (modifies globals)
 getNetworkStatus
 
 # If the retval contains "No active", we consider that an alert state and set the text color to red. Otherwise, we set it to green.
 # The symbol is set based on the type of connection found (VPN, Ethernet, Wi-Fi, or no connection)
+nicStatus="${greenCircle} ${retval}"
 if [[ "$retval" == "No active"* ]]; then
     ShowAlert=true
     nicStatus="${redCircle} ${retval}"
 elif [[ "$retval" == *"169.254"* ]]; then
     ShowAlert=true
-    nicStatus="${yellowCircle} ${retval}"
-else
-    nicStatus="${greenCircle} ${retval}"
+    nicStatus="${yellowCircle} ${retval}"    
 fi
 
 # Write output to Support App preference plist
